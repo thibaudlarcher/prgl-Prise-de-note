63a00611ebd86f624a05f645f96d312056956855
DUFOUR Arnaud <arnaud.dufour@ens.uvsq.fr>
2019-03-31 18:41:46 +0200

    Remise de code dans edition

--- src/main/java/Edition.java
+++ src/main/java/Edition.java
@@ -7,14 +7,14 @@ public class Edition {
 
     public void ProcessEdit(String str){
         if(str.equals("")){
-            String[] command = {"C:\\Program Files (x86)\\Notepad++\\notepad++","NewFile.adoc"};
+            String[] command = {"code","NewFile.adoc"};
             try {
                 Runtime.getRuntime().exec(command);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }else{
-            String[] command = {"C:\\Program Files (x86)\\Notepad++\\notepad++",str + ".adoc"};
+            String[] command = {"code",str + ".adoc"};
             try {
                 Runtime.getRuntime().exec(command);
             } catch (IOException e) {
