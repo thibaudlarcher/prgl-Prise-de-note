de7c766cd9ce1484ebaadf8b841e47f5adaa6d7b
DUFOUR Arnaud <arnaud.dufour@ens.uvsq.fr>
2019-04-05 16:53:33 +0200

    Ajout d'un titre a tout les fichiers.

--- src/main/java/Edition.java
+++ src/main/java/Edition.java
@@ -5,9 +5,27 @@ public class Edition {
     public Edition() {
     }
 
+    private void creationFile(String str){
+        String str2;
+        if(str.equals("")){
+            str2 = "NewFile";
+        }else{
+            str2 = str;
+        }
+        File f = new File(str2+".adoc");
+        if(!(f.exists())) {
+            try {
+                FileWriter fw = new FileWriter(f, true);
+                fw.write("= "+str2);
+                fw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     public void ProcessEdit(String str){
         if(programme == null){
-            System.out.println("test");
             File f = new File("notes.properties");
             try {
                 BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(f)));
@@ -20,6 +38,7 @@ public class Edition {
             }
         }
 
+        creationFile(str);
 
         if(str.equals("")){
             try {
