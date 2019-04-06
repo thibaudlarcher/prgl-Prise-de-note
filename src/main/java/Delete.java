e6e2873993cad4ece4219cfdbcbd78b64b0f8542
Jean-Charles SOTTAS <jc.sottas@gmail.com>
2019-04-06 17:36:48 +0200

    ajout paths dans delete

--- src/main/java/Delete.java
+++ src/main/java/Delete.java
@@ -10,14 +10,14 @@ public class Delete implements Command {
     @Override
     public void command(String str) {
         if (str.length() >= 5 && str.substring(str.length() - 5).equals(".adoc")) {
-            String[] command = {"rm", str};
+            String[] command = {"rm", path + str};
             try {
                 Runtime.getRuntime().exec(command);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         } else {
-            String[] command = {"rm", str + ".adoc"};
+            String[] command = {"rm", path + str + ".adoc"};
             try {
                 Runtime.getRuntime().exec(command);
             } catch (IOException e) {
