f0c4feaed05de3173de49098554de58b41c89464
DUFOUR Arnaud <arnaud.dufour@ens.uvsq.fr>
2019-04-06 22:49:43 +0200

    Modification de listing pour etre gérré par Java. Fonctionais avant en lancant un commande ls. ls n'existe pas sous windows ce qui pose des probleme.

--- src/main/java/Listing.java
+++ src/main/java/Listing.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
@@ -27,14 +28,19 @@ public class Listing implements Command {
 
     @Override
     public void command(String str) {
-        String[] command = {"ls","*.adoc",paths};
-        Process process;
-        try {
-            process = Runtime.getRuntime().exec(command);
-            affichageList(process);
-        } catch (IOException e) {
-            e.printStackTrace();
+        StringBuilder output = new StringBuilder();
+
+        System.out.println("\033[0;34m");
+        output.append(paths);
+        output.append(" :\n");
+        File f = new File(paths);
+        for(File f2 : f.listFiles()){
+            output.append("|    ");
+            output.append(f2.getName());
+            output.append("\n");
         }
+        System.out.println(output);
+        System.out.println("\033[0m");
     }
 }
 
